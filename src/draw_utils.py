a1e40d13735adf9081a38e4ed35e99af47f914df
Henno Théo <94365544+NazioLT@users.noreply.github.com>
2025-05-09 20:10:22 +0200

    :sparkles: Add Distribution graph

---
+++ src/draw_utils.py
@@ -0,0 +1,10 @@
+﻿import matplotlib.pyplot as plt
+
+def draw_pie_chart(values, value_labels, title):
+    plt.figure(figsize=(4, 4))
+    plt.pie(values, labels=value_labels, autopct='%1.1f%%', startangle=90)
+    plt.title(title)
+
+def save_png(filename):
+    plt.savefig(F"output/{filename}", format='png', dpi=300)
+    plt.close()
---
+++ src/query_utils.py
@@ -0,0 +1,6 @@
+﻿def filter_with_other_category(data, min_percent):
+    type_counts = data.value_counts(normalize=True) * 100
+
+    type_counts_filtered = type_counts[type_counts >= min_percent]
+    type_counts_filtered["Other"] = type_counts[type_counts < min_percent].sum()
+    return type_counts_filtered
